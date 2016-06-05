04aeb62ea63de19c89a142935ce5bc9c99e54b96
Justin Yu <justin.v.yu@gmail.com>
2016-06-05 11:50:30 -0700

    Update Test.h

--- classes/Test.h
+++ classes/Test.h
@@ -14,18 +14,8 @@
 // Define a "class" for your robot
 
 typedef struct MyClass {
-    Controller controller; // refers to the wallaby controller
-    Create create; // refers to your create
-    // Camera camera; // uncomment if you have a camera
-
-    // Custom properties/instance methods
-
     int a_property;
-
-    // instance methods must be listed as FUNCTION POINTERS
-    // that's what the little star (*) is for
     void (*an_instance_method)();
-
 } MyClass;
 
 extern MyClass new_robot(int a_property); // constructor
