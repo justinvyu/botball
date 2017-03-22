9d3a7c1fd48c90822d41654035aaab73d70d7308
Justin Yu <justin.v.yu@gmail.com>
2017-03-22 11:42:25 -0700

    Update pid_training.c

--- other/pid_training.c
+++ other/pid_training.c
@@ -6,6 +6,43 @@ enum Side {
     RIGHT = 1
 };
 
+/**
+ *  line_follow(int distance_in_ticks) function by Justin Yu 3/14 
+ *  --------------------------------------------------------------
+ *  This function uses a PID controller to follow a line for a
+ *  specified distance, stopping when the motor position counter
+ *  reaches the input `distance_in_ticks` parameter. The P controller calculates
+ *  the error between the current sensor value and the desired threshold
+ *  value. The I controller calculates a running total of error. The
+ *  D controller calculates the change in error from iteration to iteration.
+ *  These three, multiplied by experimentally defined constants and summed,
+ *  create a turn variable that alters the speed of both motors (0 and 1).
+ */
+void line_follow(int distance_in_ticks) {
+    float kp = 2.4,   // proportional constant
+          ki = 0.1,   // integral constant
+          kd = 8.2;   // derivative constant
+
+    int last_error = 0;
+    int speed = 1000;    // velocity of motors at error = 0
+    int threshold = 800; // experimentally determined white/black threshold
+    
+    clear_motor_position_counter(0);
+    while(get_motor_position_counter(0) < distance_in_ticks) {
+        sensor_value = analog10(tophat);
+
+        error = sensor_value - threshold;            // P
+        integral = (7. / 8.) * integral + error;     // I
+        derivative = error - last_error;             // D
+
+        turn = (int)(kp * error + ki * integral + kd * derivative);
+        mav(0, speed - turn); // power left motor
+        mav(1, speed + turn); // power right motor
+        last_error = error;
+        msleep(1);
+    }
+}
+
 int main()
 {
     controller = new_controller(0, 1, 14.5, 5.0);
